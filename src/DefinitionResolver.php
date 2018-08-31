c88b41b1d40e44c49fd9a31baf9f425a7ec65786
Dylan McGannon <me@dyl.dog>
2018-08-31 22:28:37 +1000

    fix(indexing): Files with classes that declare itself to be it's parent can cause an infinite loop when indexing. #669

--- src/DefinitionResolver.php
+++ src/DefinitionResolver.php
@@ -471,7 +471,9 @@ class DefinitionResolver
             // Repeat for parent class
             if ($implementorDef->extends) {
                 foreach ($implementorDef->extends as $extends) {
-                    $implementorFqns[] = $extends;
+                    if ($extends !== $implementorFqn) {
+                        $implementorFqns[] = $extends;
+                    }
                 }
             }
         }
